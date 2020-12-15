49fdd5a6a0560391fbdec5a0714ca030525da93b
Victor L. Santos <victorluissantos@live.com>
2020-12-15 19:30:47 -0300

    funcao calcula idade

--- 2 semana/exercicios.sql
+++ 2 semana/exercicios.sql
@@ -35,8 +35,39 @@ END
 	    lances l ON a.id = l.anuncio_id;
 
 -- 3 função simples que retorna a idade do usuário (só analizar o ano e o mês)
+CREATE FUNCTION `getIdade` (data_nascimento DATE)
+RETURNS INTEGER
+BEGIN
+	
+    DECLARE idade INT DEFAULT 0;
+    DECLARE ano INT DEFAULT 0;
+    DECLARE mes INT DEFAULT 0;
+    DECLARE cano INT DEFAULT 0;
+    DECLARE cmes INT DEFAULT 0;
+    
+    SET ano = YEAR(data_nascimento);
+    SET mes = MONTH(data_nascimento);
+    
+    SET cano = YEAR(curdate());
+    SET cmes = MONTH(curdate());
+    
+    IF cmes > mes THEN
+		SET idade = cano-ano;
+	ELSE
+		SET idade = cano-ano-1;
+    END IF;
+    
+	RETURN idade;
+END
 
 -- 4 qual o usuário mais velho do sistema
+SELECT 
+    u.*,
+    GETIDADE(u.data_nascimento) as `idade`
+FROM
+    usuarios u
+ORDER BY 8 DESC
+LIMIT 1;
 
 -- 5 função que retorna qual a soma dos anuncios de um determinado usuário
 CREATE FUNCTION `soma_anuncio` (pusuario_id  INT)
